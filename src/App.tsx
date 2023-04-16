a6c83713497c8c867afdada734778779f0a2b7d3
ismaelpaul <ismaafp@gmail.com>
2023-04-16 20:34:41 +0100

    Add "/" router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+
 function App() {
-	return;
+	return (
+		<Routes>
+			<Route path="/" element={<Nav />}></Route>
+		</Routes>
+	);
 }
 
 export default App;
