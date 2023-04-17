607cbb8d66153cfadb4ceb3dd6e0940f0df27264
ismaelpaul <ismaafp@gmail.com>
2023-04-17 15:09:24 +0100

    Add reusable Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ButtonProps {
+	buttonText: string;
+	buttonClass: string;
+}
+const Button = ({ buttonText, buttonClass }: ButtonProps) => {
+	return <button className={buttonClass}>{buttonText}</button>;
+};
+
+export default Button;
