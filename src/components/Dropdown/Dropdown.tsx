40ea7c876715e0dcd437752100cdcdf0b2b0cfa9
ismaelpaul <ismaafp@gmail.com>
2023-04-18 15:06:17 +0100

    Add Dropdown reusable component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import ArrowUp from '../SVGComponents/ArrowUp';
+import ArrowDown from '../SVGComponents/ArrowDown';
+import Check from '../SVGComponents/Check';
+
+interface Options {
+	label: string;
+	value: string;
+}
+interface DropdownProps {
+	options: Options[];
+	defaultSelectedOption: Options;
+}
+
+const Dropdown = ({ options, defaultSelectedOption }: DropdownProps) => {
+	const [selectedOption, setSelectedOption] = useState(defaultSelectedOption);
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div>
+			<div className="flex items-center gap-2 relative">
+				<span
+					className={
+						isOpen
+							? 'flex gap-1 text-white-smoke opacity-75 '
+							: 'flex gap-1 text-white-smoke'
+					}
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<p>Sort by :</p>{' '}
+					<strong>
+						{selectedOption ? selectedOption.label : 'Select an option'}
+					</strong>
+				</span>
+				{isOpen ? (
+					<ArrowUp className="stroke-white" />
+				) : (
+					<ArrowDown className="stroke-white" />
+				)}
+				{isOpen && (
+					<ul className="bg-white absolute w-48 h-max top-12 divide-y divide-dark-slate-blue/10  rounded-xl shadow-[0_10px_40px_-7px_rgba(55,63,104,0.35)]">
+						{options.map((option, index) => {
+							return (
+								<>
+									<li
+										key={index}
+										className="text-light-slate-blue flex items-center px-6 py-3 "
+									>
+										{option.label}
+										{selectedOption &&
+											selectedOption.value === option.value && <Check />}
+									</li>
+								</>
+							);
+						})}
+					</ul>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Dropdown;
