fb5c9de9923847753c0a50f268c2b0c777726069
ismaelpaul <ismaafp@gmail.com>
2023-04-23 20:07:15 +0100

    Add getAllProductRequests api function

---
+++ src/utils/api.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+interface AxiosConfig extends AxiosRequestConfig {
+	credentials?: string;
+}
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+const productFeedbackApi = axios.create({
+	baseURL: `${baseUrl}`,
+	withCredentials: true,
+	credentials: 'include',
+} as AxiosConfig);
+
+export const getAllProductRequests = async () => {
+	try {
+		const response = await productFeedbackApi.get('/product-requests');
+
+		return response.data;
+	} catch (error: any) {
+		return error.response.data.message;
+	}
+};
