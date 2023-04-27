1a91dc40e8acd28640f769c788920dffd3324053
ismaelpaul <ismaafp@gmail.com>
2023-04-27 09:07:32 +0100

    Add InitialState interface for sidebar state

--- src/interfaces/IInitialState.ts
+++ src/interfaces/IInitialState.ts
@@ -1,7 +1,11 @@
-export interface InitialState {
+export interface InitialStateProdRequests {
 	productRequests: [];
 	isError: boolean;
 	isSuccess: boolean;
 	isLoading: boolean;
 	message: unknown;
+}
+
+export interface InitialStateSidebar {
+	isSidebarOpen: boolean;
 }
