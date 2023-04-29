09358bde7c22b2c58ea341d5fe7b7b8b73f76df0
ismaelpaul <ismaafp@gmail.com>
2023-04-29 22:19:03 +0100

    Add FeedbackDetailPage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import SuggestionsPage from './pages/Suggestions/SuggestionsPage';
+import FeedbackDetailPage from './pages/FeedbackDetail/FeedbackDetailPage';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<SuggestionsPage />}></Route>
+			<Route
+				path="/product-requests/:request_id"
+				element={<FeedbackDetailPage />}
+			></Route>
 		</Routes>
 	);
 }
