d2b24a02f13c1f6b1a50dc6dac35c1c89cc5e77b
ismaelpaul <ismaafp@gmail.com>
2023-04-29 22:14:06 +0100

    Add filteredRequestsSlice

---
+++ src/redux/features/filteredRequests/filteredRequestsSlice.ts
@@ -0,0 +1,66 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { ProductRequests } from '../../../interfaces/IProductRequests';
+import { InitialStateFilteredRequests } from '../../../interfaces/IInitialState';
+
+const initialState: InitialStateFilteredRequests = {
+	inProgress: [],
+	planned: [],
+	live: [],
+};
+const filteredRequestsSlice = createSlice({
+	name: 'filteredRequests',
+	initialState,
+	reducers: {
+		IN_PROGRESS_REQUESTS(state, action) {
+			const productRequests = action.payload;
+
+			const inProgressFiltered: ProductRequests[] = productRequests.filter(
+				(request: ProductRequests) => {
+					if (request.status === 'in-progress') {
+						return request;
+					}
+				}
+			);
+			state.inProgress = inProgressFiltered;
+		},
+		PLANNED_REQUESTS(state, action) {
+			const productRequests = action.payload;
+
+			const plannedFiltered: ProductRequests[] = productRequests.filter(
+				(request: ProductRequests) => {
+					if (request.status === 'planned') {
+						return request;
+					}
+				}
+			);
+			state.planned = plannedFiltered;
+		},
+		LIVE_REQUESTS(state, action) {
+			const productRequests = action.payload;
+
+			const liveFiltered: ProductRequests[] = productRequests.filter(
+				(request: ProductRequests) => {
+					if (request.status === 'live') {
+						return request;
+					}
+				}
+			);
+			state.live = liveFiltered;
+		},
+	},
+});
+
+export const { IN_PROGRESS_REQUESTS, PLANNED_REQUESTS, LIVE_REQUESTS } =
+	filteredRequestsSlice.actions;
+
+export const selectInProgressFilteredRequests = (
+	state: InitialStateFilteredRequests
+) => state.inProgress;
+
+export const selectPlannedRequests = (state: InitialStateFilteredRequests) =>
+	state.planned;
+
+export const selectLiveRequests = (state: InitialStateFilteredRequests) =>
+	state.live;
+
+export default filteredRequestsSlice.reducer;
