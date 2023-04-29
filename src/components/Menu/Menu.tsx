8a7161cadaae42803179c75e14c5b2d467a63c37
ismaelpaul <ismaafp@gmail.com>
2023-04-29 22:17:20 +0100

    Add conditional rendering icon depending on sidebar state

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,21 @@
-import { hamburguer } from '../../data/icons';
+import { useDispatch, useSelector } from 'react-redux';
 import bgImage from '../../assets/suggestions/mobile/background-header.png';
+import Hamburguer from '../SVGComponents/Hamburguer';
+import {
+	SET_SIDEBAR,
+	selectIsSidebarOpen,
+} from '../../redux/features/sidebar/sidebarSlice';
+import CloseMenu from '../SVGComponents/CloseMenu';
 
 const Menu = () => {
+	const isSidebarOpen = useSelector(selectIsSidebarOpen);
+
+	const dispatch = useDispatch();
+
+	const toggleSidebar = () => {
+		dispatch(SET_SIDEBAR(!isSidebarOpen));
+	};
+
 	return (
 		<header className="flex items-center tablet:hidden">
 			<div className="w-full flex justify-between items-center p-6 absolute">
@@ -11,7 +25,9 @@ const Menu = () => {
 					</h1>
 					<p className="text-subtitleMobile opacity-75">Feedback Board</p>
 				</div>
-				<span>{hamburguer}</span>
+				<div onClick={toggleSidebar}>
+					{isSidebarOpen ? <CloseMenu /> : <Hamburguer />}
+				</div>
 			</div>
 			<img src={bgImage} />
 		</header>
