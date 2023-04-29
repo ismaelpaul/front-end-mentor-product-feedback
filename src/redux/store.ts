5e93d331001ce5d8797a5d87db7b94fcb6fc1bc6
ismaelpaul <ismaafp@gmail.com>
2023-04-29 22:17:58 +0100

    Add filteredRequestsReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productRequestsReducer from './features/productRequests/productRequestsSlice';
 import sidebarReducer from './features/sidebar/sidebarSlice';
+import filteredRequestsReducer from './features/filteredRequests/filteredRequestsSlice';
 
 export const store = configureStore({
-	reducer: { productRequests: productRequestsReducer, sidebar: sidebarReducer },
+	reducer: {
+		productRequests: productRequestsReducer,
+		sidebar: sidebarReducer,
+		filteredRequests: filteredRequestsReducer,
+	},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
