6907e2e51d63be5c9217968b145513e4c0b6ba7c
ismaelpaul <ismaafp@gmail.com>
2023-04-30 22:30:34 +0100

    Add LinkProps type and <Link> element

--- src/components/GoBackLink/GoBackLink.tsx
+++ src/components/GoBackLink/GoBackLink.tsx
@@ -1,13 +1,19 @@
+import { Link } from 'react-router-dom';
 import ArrowLeft from '../SVGComponents/ArrowLeft';
 
-const GoBackLink = () => {
+type LinkProps = {
+	link: string;
+};
+const GoBackLink = ({ link }: LinkProps) => {
 	return (
-		<nav className="flex items-center gap-4">
-			<ArrowLeft />
-			<span className="text-light-slate-blue text-subtitleMobile font-jost font-bold">
-				Go Back
-			</span>
-		</nav>
+		<Link to={link}>
+			<nav className="flex items-center gap-4">
+				<ArrowLeft />
+				<span className="text-light-slate-blue text-subtitleMobile font-jost font-bold">
+					Go Back
+				</span>
+			</nav>
+		</Link>
 	);
 };
 
