77b8d6b56d150b193ae96c647f8bd0931703c50f
ismaelpaul <ismaafp@gmail.com>
2023-05-02 21:19:44 +0100

    Add conditional rendering sidebar

--- src/components/SuggestionsBar/SuggestionsBar.tsx
+++ src/components/SuggestionsBar/SuggestionsBar.tsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
 import Button from '../Button/Button';
 import Dropdown from '../Dropdown/Dropdown';
+import { selectIsSidebarOpen } from '../../redux/features/sidebar/sidebarSlice';
+import Sidebar from '../Sidebar/Sidebar';
 
 const SuggestionsBar = () => {
+	const isSidebarOpen = useSelector(selectIsSidebarOpen);
+
 	const options = [
 		{ label: 'Most Upvotes', value: 'upvotes' },
 		{ label: 'Least Upvotes', value: 'upvotes' },
@@ -13,8 +18,18 @@ const SuggestionsBar = () => {
 	const buttonClass = 'bg-purple font-semiBold rounded-lg w-32 h-10';
 
 	return (
-		<div className="h-14 bg-dark-gray-blue">
-			<div className="text-white text-subtitleMobile font-jost h-full flex items-center justify-between px-6">
+		<div>
+			<div
+				className={` fixed w-full h-full z-10 ${
+					isSidebarOpen
+						? 'bg-black/50 flex justify-end'
+						: ' delay-500 bg-black/0 hidden'
+				}`}
+			>
+				<Sidebar />
+			</div>
+
+			<div className="text-white text-subtitleMobile font-jost h-14 bg-dark-gray-blue flex items-center justify-between px-6">
 				<div className="flex gap-2 ">
 					<Dropdown options={options} defaultSelectedOption={options[0]} />
 				</div>
