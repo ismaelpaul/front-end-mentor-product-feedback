a328736345e3601c614ac2ede2e0287149d78279
ismaelpaul <ismaafp@gmail.com>
2023-05-03 19:26:21 +0100

    Move logic from FeedbackCard to FeedbackList component

--- src/components/Feedback/FeedbackList.tsx
+++ src/components/Feedback/FeedbackList.tsx
@@ -1,10 +1,46 @@
+import { useEffect } from 'react';
 import FeedbackCard from './FeedbackCard';
+import {
+	getProductRequest,
+	getProductRequests,
+} from '../../redux/features/productRequests/productRequestsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../redux/store';
+import FeedbackEmpty from './FeedbackEmpty';
+import { ProductRequests } from '../../interfaces/IProductRequests';
+import { useNavigate } from 'react-router-dom';
 
 const FeedbackList = () => {
+	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+
+	const { productRequests } = useSelector(
+		(state: RootState) => state.productRequests
+	);
+
+	const getSingleRequest = async (id: string) => {
+		const productRequest = await dispatch(getProductRequest(id));
+
+		navigate(`/product-requests/${productRequest.payload._id}`);
+	};
+
+	useEffect(() => {
+		dispatch(getProductRequests());
+	}, []);
+
 	return (
-		<section>
-			<FeedbackCard />
-		</section>
+		<main className="my-8 mx-6">
+			{productRequests.length === 0 && <FeedbackEmpty />}
+			<section className="flex flex-col gap-4">
+				{productRequests.map((singleRequest: ProductRequests) => {
+					return (
+						<article onClick={() => getSingleRequest(singleRequest._id)}>
+							<FeedbackCard singleRequest={singleRequest} />
+						</article>
+					);
+				})}
+			</section>
+		</main>
 	);
 };
 
