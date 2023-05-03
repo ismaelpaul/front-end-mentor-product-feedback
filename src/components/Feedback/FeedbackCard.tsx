42d19abfa14a57edb7f845dc24d51d12f1892550
ismaelpaul <ismaafp@gmail.com>
2023-05-03 11:36:47 +0100

    Add repliesCount function and reorganize component structure

--- src/components/Feedback/FeedbackCard.tsx
+++ src/components/Feedback/FeedbackCard.tsx
@@ -1,64 +1,58 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../redux/store';
-import { getProductRequests } from '../../redux/features/productRequests/productRequestsSlice';
 import Card from '../Card/Card';
 import ArrowUp from '../SVGComponents/ArrowUp';
 import Comments from '../SVGComponents/Comments';
 import { ProductRequests } from '../../interfaces/IProductRequests';
-import FeedbackEmpty from './FeedbackEmpty';
 
-const FeedbackCard = () => {
-	const dispatch = useDispatch<AppDispatch>();
+type FeedbackCardPros = {
+	singleRequest: ProductRequests;
+};
 
-	const { productRequests } = useSelector(
-		(state: RootState) => state.productRequests
-	);
-
-	useEffect(() => {
-		dispatch(getProductRequests());
-	}, []);
-
+const FeedbackCard = ({ singleRequest }: FeedbackCardPros) => {
+	const repliesCount = () => {
+		return (
+			singleRequest.comments?.reduce((count, comment) => {
+				if (comment.replies?.length !== undefined) {
+					return count + comment.replies?.length;
+				}
+				return count;
+			}, 0) || 0
+		);
+	};
 	return (
-		<article className="flex flex-col gap-4 mx-6 mt-8 mb-14">
-			{productRequests.length === 0 && <FeedbackEmpty />}
-			{productRequests.map((request: ProductRequests, index) => {
-				return (
-					<Card key={index} cardClass="w-full bg-white font-jost rounded-lg ">
-						<div className=" p-6">
-							<div>
-								<h1 className="text-dark-slate-blue text-subtitleMobile font-bold tracking-tight ">
-									{request.title}
-								</h1>
-								<p className="text-light-slate-blue text-subtitleMobile font-regular mt-2">
-									{request.description}
-								</p>
-								<div className="bg-white-smoke w-fit rounded-lg mt-2">
-									<span className="text-blue font-semiBold text-subtitleMobile px-4 py-2">
-										{request.category.charAt(0).toUpperCase() +
-											request.category.slice(1)}
-									</span>
-								</div>
-							</div>
-							<div className="flex justify-between mt-4">
-								<div className="flex items-center gap-2.5 bg-white-smoke w-fit px-4 py-2 rounded-lg">
-									<ArrowUp />
-									<span className="text-dark-slate-blue text-subtitleMobile font-semiBold tracking-tight">
-										{request.upvotes}
-									</span>
-								</div>
-								<div className="flex items-center gap-2.5">
-									<Comments />
-									<span className="text-dark-slate-blue text-subtitleMobile font-semiBold">
-										{request.comments ? request.comments.length : 0}
-									</span>
-								</div>
-							</div>
-						</div>
-					</Card>
-				);
-			})}
-		</article>
+		<Card cardClass="bg-white font-jost rounded-lg ">
+			<div className=" p-6">
+				<div>
+					<h1 className="text-dark-slate-blue text-subtitleMobile font-bold tracking-tight ">
+						{singleRequest.title}
+					</h1>
+					<p className="text-light-slate-blue text-subtitleMobile font-regular mt-2">
+						{singleRequest.description}
+					</p>
+					<div className="bg-white-smoke w-fit rounded-lg mt-2">
+						<span className="text-blue font-semiBold text-subtitleMobile px-4 py-2">
+							{singleRequest.category.charAt(0).toUpperCase() +
+								singleRequest.category.slice(1)}
+						</span>
+					</div>
+				</div>
+				<div className="flex justify-between mt-4">
+					<div className="flex items-center gap-2.5 bg-white-smoke w-fit px-4 py-2 rounded-lg">
+						<ArrowUp />
+						<span className="text-dark-slate-blue text-subtitleMobile font-semiBold tracking-tight">
+							{singleRequest.upvotes}
+						</span>
+					</div>
+					<div className="flex items-center gap-2.5">
+						<Comments />
+						<span className="text-dark-slate-blue text-subtitleMobile font-semiBold">
+							{singleRequest.comments
+								? singleRequest.comments.length + repliesCount()
+								: 0}
+						</span>
+					</div>
+				</div>
+			</div>
+		</Card>
 	);
 };
 
