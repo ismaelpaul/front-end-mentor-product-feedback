3528e1491e40d7b74e88ca64b52968f4563fd01c
ismaelpaul <ismaafp@gmail.com>
2023-05-03 19:23:31 +0100

    Add NewFeedback page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import SuggestionsPage from './pages/Suggestions/SuggestionsPage';
 import FeedbackDetailPage from './pages/FeedbackDetail/FeedbackDetailPage';
+import NewFeedbackPage from './pages/NewFeedback/NewFeedbackPage';
 
 function App() {
 	return (
@@ -9,6 +10,10 @@ function App() {
 			<Route
 				path="/product-requests/:request_id"
 				element={<FeedbackDetailPage />}
+			></Route>
+			<Route
+				path="/product-requests/new-feedback"
+				element={<NewFeedbackPage />}
 			></Route>
 		</Routes>
 	);
