efa31a4599372cda76ea0f151d93dee6041f90a3
ismaelpaul <ismaafp@gmail.com>
2023-05-04 21:46:49 +0100

    Add RoadmapPage

---
+++ src/pages/Roadmap/RoadmapPage.tsx
@@ -0,0 +1,13 @@
+import NavRoadmap from '../../components/Nav/NavRoadmap';
+import Tab from '../../components/Tab/Tab';
+
+const RoadmapPage = () => {
+	return (
+		<>
+			<NavRoadmap />
+			<Tab />
+		</>
+	);
+};
+
+export default RoadmapPage;
