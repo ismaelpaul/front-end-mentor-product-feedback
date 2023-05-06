dc26bdd146fdfab76dd7e853b8c41005f147bc4a
ismaelpaul <ismaafp@gmail.com>
2023-05-06 21:32:35 +0100

    Add RoadmapPage route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import SuggestionsPage from './pages/Suggestions/SuggestionsPage';
 import FeedbackDetailPage from './pages/FeedbackDetail/FeedbackDetailPage';
 import NewFeedbackPage from './pages/NewFeedback/NewFeedbackPage';
+import RoadmapPage from './pages/Roadmap/RoadmapPage';
 
 function App() {
 	return (
@@ -11,10 +12,8 @@ function App() {
 				path="/product-requests/:request_id"
 				element={<FeedbackDetailPage />}
 			></Route>
-			<Route
-				path="/product-requests/new-feedback"
-				element={<NewFeedbackPage />}
-			></Route>
+			<Route path="/new-feedback" element={<NewFeedbackPage />}></Route>
+			<Route path="/roadmap" element={<RoadmapPage />}></Route>
 		</Routes>
 	);
 }
