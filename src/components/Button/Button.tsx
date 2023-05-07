f533da25d306d746a30bf5acee48484e954a9567
ismaelpaul <ismaafp@gmail.com>
2023-05-07 13:21:59 +0100

    Add type to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 interface ButtonProps {
 	buttonText: string;
 	buttonClass: string;
+	type?: 'button' | 'submit';
 }
-const Button = ({ buttonText, buttonClass }: ButtonProps) => {
-	return <button className={buttonClass}>{buttonText}</button>;
+const Button = ({ buttonText, buttonClass, type = 'button' }: ButtonProps) => {
+	return (
+		<button type={type} className={buttonClass} aria-label={buttonText}>
+			{buttonText}
+		</button>
+	);
 };
 
 export default Button;
