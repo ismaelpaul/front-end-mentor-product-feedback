10bf06fa76ea815ce02fca29e6759fe24fde5091
ismaelpaul <ismaafp@gmail.com>
2023-05-07 13:21:26 +0100

    Add goBackClass utility class

--- src/pages/NewFeedback/NewFeedbackPage.tsx
+++ src/pages/NewFeedback/NewFeedbackPage.tsx
@@ -1,11 +1,14 @@
-import CreateFeedback from '../../components/Feedback/CreateFeedback';
+import CreateFeedback from '../../components/Feedback/NewFeedback/CreateFeedback';
 import GoBackLink from '../../components/GoBackLink/GoBackLink';
 
 const NewFeedbackPage = () => {
 	const link = '/';
+	const goBackClass =
+		'text-light-slate-blue text-subtitleMobile font-jost font-bold';
+
 	return (
 		<div className="gap-6 m-6">
-			<GoBackLink link={link} />
+			<GoBackLink goBackClass={goBackClass} link={link} />
 			<CreateFeedback />
 		</div>
 	);
