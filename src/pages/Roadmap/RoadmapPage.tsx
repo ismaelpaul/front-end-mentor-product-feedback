0a0e4550e7b01565400d2ef57f5b25d37e3cce04
ismaelpaul <ismaafp@gmail.com>
2023-05-08 09:05:27 +0100

    Lift selectedTab state to RoadmapPage and pass it down as props

--- src/pages/Roadmap/RoadmapPage.tsx
+++ src/pages/Roadmap/RoadmapPage.tsx
@@ -1,11 +1,51 @@
+import { useState } from 'react';
+import FeedbackCard from '../../components/Feedback/FeedbackCard';
 import NavRoadmap from '../../components/Nav/NavRoadmap';
 import Tab from '../../components/Tab/Tab';
+import { ProductRequests } from '../../interfaces/IProductRequests';
+import RoadmapHeading from '../../components/Roadmap/RoadmapHeading';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 
 const RoadmapPage = () => {
+	const [selectedTab, setSelectedTab] = useState({
+		name: 'Planned',
+		content: '',
+		index: 0,
+	});
+
+	const storedRequests = localStorage.getItem('productRequests');
+	const productRequests = storedRequests ? JSON.parse(storedRequests) : [];
+
+	const inProgressContent = useSelector(
+		(state: RootState) => state.filteredRequests.inProgress
+	);
+	const plannedContent = useSelector(
+		(state: RootState) => state.filteredRequests.planned
+	);
+
+	const liveContent = useSelector(
+		(state: RootState) => state.filteredRequests.live
+	);
+
 	return (
 		<>
 			<NavRoadmap />
-			<Tab />
+			<Tab selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+			<RoadmapHeading
+				selectedTab={selectedTab}
+				inProgressContent={inProgressContent}
+				plannedContent={plannedContent}
+				liveContent={liveContent}
+			/>
+
+			{productRequests.map((request: ProductRequests) => {
+				return request.status === selectedTab.name.toLowerCase() ? (
+					<main key={request._id} className="my-6 mx-6 flex flex-col gap-4">
+						<FeedbackCard singleRequest={request} />
+					</main>
+				) : null;
+			})}
 		</>
 	);
 };
