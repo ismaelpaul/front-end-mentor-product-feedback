66399dea9208c5e45b8ce85187f03b52ef18fc57
ismaelpaul <ismaafp@gmail.com>
2023-05-12 10:54:39 +0100

    Add goBackClass and change div for header tag

--- src/components/Nav/NavFeedbackDetail.tsx
+++ src/components/Nav/NavFeedbackDetail.tsx
@@ -1,18 +1,27 @@
+import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import GoBackLink from '../GoBackLink/GoBackLink';
 
 const NavFeedbackDetail = () => {
+	const goBackClass =
+		'text-light-slate-blue text-subtitleMobile font-jost font-bold';
+	const arrowClass = '';
 	const buttonText = 'Edit Feedback';
 	const buttonClass =
 		'bg-blue text-white font-jost font-semiBold text-subtitleMobile rounded-lg w-32 h-10';
 
 	const link = '/';
 	return (
-		<div className="flex justify-between items-center">
-			<GoBackLink link={link} />
-
-			<Button buttonText={buttonText} buttonClass={buttonClass} />
-		</div>
+		<header className="flex justify-between items-center">
+			<GoBackLink
+				goBackClass={goBackClass}
+				arrowClass={arrowClass}
+				link={link}
+			/>
+			<Link to={'/edit-feedback'}>
+				<Button buttonText={buttonText} buttonClass={buttonClass} />
+			</Link>
+		</header>
 	);
 };
 
