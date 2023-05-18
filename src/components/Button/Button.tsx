acfb857206ed76bffe1c51b279f9545362d6ea14
ismaelpaul <ismaafp@gmail.com>
2023-05-18 14:07:45 +0100

    Add form and onClick interface properties

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,24 @@ interface ButtonProps {
 	buttonText: string;
 	buttonClass: string;
 	type?: 'button' | 'submit';
+	form?: string;
+	onClick?: () => void;
 }
-const Button = ({ buttonText, buttonClass, type = 'button' }: ButtonProps) => {
+const Button = ({
+	buttonText,
+	buttonClass,
+	type = 'button',
+	form,
+	onClick,
+}: ButtonProps) => {
 	return (
-		<button type={type} className={buttonClass} aria-label={buttonText}>
+		<button
+			type={type}
+			className={buttonClass}
+			aria-label={buttonText}
+			form={form}
+			onClick={onClick}
+		>
 			{buttonText}
 		</button>
 	);
