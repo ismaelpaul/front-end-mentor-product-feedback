4508d0f6ee5099074b534dc41fdbefea0a2c912c
ismaelpaul <ismaafp@gmail.com>
2023-05-22 18:47:04 +0100

    Add form validation schema and submit function

--- src/components/Feedback/NewFeedback/CreateFeedback.tsx
+++ src/components/Feedback/NewFeedback/CreateFeedback.tsx
@@ -1,9 +1,19 @@
+import { ZodType, z } from 'zod';
 import Button from '../../Button/Button';
 import Card from '../../Card/Card';
 import NewFeedback from '../../SVGComponents/NewFeedback';
-import Category from './Category';
-import FeedbackDetail from './FeedbackDetail';
-import FeedbackTitle from './FeedbackTitle';
+import FeedbackForm from '../FeedbackForm';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	SET_SELECTED_OPTION_FORM,
+	addProductRequest,
+	selectOptionForm,
+} from '../../../redux/features/productRequests/productRequestsSlice';
+import { ProductRequests } from '../../../interfaces/IProductRequests';
+import { AppDispatch } from '../../../redux/store';
+import { useNavigate } from 'react-router';
 
 const CreateFeedback = () => {
 	const cardClass = 'bg-white font-jost rounded-lg mt-14';
@@ -11,6 +21,40 @@ const CreateFeedback = () => {
 	const buttonTextCancel = 'Cancel';
 	const buttonClass =
 		'text-white-smoke text-subtitleMobile font-semiBold rounded-lg w-full h-10';
+
+	const selectedOption = useSelector(selectOptionForm);
+
+	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+
+	const schema: ZodType<ProductRequests> = z.object({
+		title: z.string().min(3).max(60),
+		category: z.string(),
+		description: z.string().min(5).max(250),
+		upvotes: z.number(),
+		status: z.string(),
+	});
+
+	const { register, handleSubmit, reset } = useForm<ProductRequests>({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			title: '',
+			category: '',
+			description: '',
+			upvotes: 0,
+			status: 'planned',
+		},
+	});
+
+	const submitData = async (data: ProductRequests) => {
+		data.category = selectedOption.value;
+		dispatch(SET_SELECTED_OPTION_FORM({ label: 'Feature', value: 'feature' }));
+		await dispatch(addProductRequest(data));
+		reset();
+		navigate('/');
+	};
+
+	const onSubmit = handleSubmit(submitData);
 
 	return (
 		<main>
@@ -22,11 +66,11 @@ const CreateFeedback = () => {
 					<h1 className="text-dark-slate-blue text-title18px font-bold tracking-tightier">
 						Create new Feedback
 					</h1>
-					<FeedbackTitle />
-					<Category />
-					<FeedbackDetail />
+					<FeedbackForm register={register} onSubmit={onSubmit} />
 					<div className="flex flex-col gap-4 mt-10">
 						<Button
+							type={'submit'}
+							form={'feedback-form'}
 							buttonClass={`bg-purple ${buttonClass}`}
 							buttonText={buttonTextAdd}
 						/>
