b11192f3b69f98dd8736def9e028ed672137a2c3
ismaelpaul <ismaafp@gmail.com>
2023-06-11 21:12:02 +0100

    Add fixed utility class
    This prevent scrolling of the CommentList component in the background

--- src/pages/Suggestions/SuggestionsPage.tsx
+++ src/pages/Suggestions/SuggestionsPage.tsx
@@ -1,9 +1,12 @@
+import { useSelector } from 'react-redux';
 import FeedbackList from '../../components/Feedback/FeedbackList';
 import Nav from '../../components/Nav/Nav';
+import { selectIsSidebarOpen } from '../../redux/features/sidebar/sidebarSlice';
 
 const SuggestionsPage = () => {
+	const isSidebarOpen = useSelector(selectIsSidebarOpen);
 	return (
-		<section>
+		<section className={isSidebarOpen ? 'fixed' : ''}>
 			<Nav />
 			<FeedbackList />
 		</section>
