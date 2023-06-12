c1ce0dc8dfb1cd232672857967fa2f86bd49bb05
ismaelpaul <ismaafp@gmail.com>
2023-06-12 21:44:44 +0100

    Add tablet media query and change html structure to add elements

--- src/components/SuggestionsBar/SuggestionsBar.tsx
+++ src/components/SuggestionsBar/SuggestionsBar.tsx
@@ -4,6 +4,8 @@ import Dropdown from '../Dropdown/DropdownSuggestions';
 import { selectIsSidebarOpen } from '../../redux/features/sidebar/sidebarSlice';
 import Sidebar from '../Sidebar/Sidebar';
 import { Link } from 'react-router-dom';
+import Suggestions from '../SVGComponents/Suggestions';
+import { RootState } from '../../redux/store';
 
 const SuggestionsBar = () => {
 	const options = [
@@ -16,27 +18,40 @@ const SuggestionsBar = () => {
 	const isSidebarOpen = useSelector(selectIsSidebarOpen);
 
 	const buttonText = '+ Add Feedback';
-	const buttonClass = 'bg-purple font-semiBold rounded-lg w-32 h-10';
+	const buttonClass =
+		'bg-purple font-semiBold rounded-lg w-32 h-10 tablet:h-11';
+
+	const suggestions = useSelector(
+		(state: RootState) => state.filteredRequests.suggestion
+	);
 
 	return (
 		<div>
 			<div
-				className={` fixed w-full h-full z-10 ${
+				className={` fixed w-screen h-screen z-10 ${
 					isSidebarOpen
 						? 'bg-black/50 flex justify-end'
 						: ' delay-500 bg-black/0 hidden'
-				}`}
+				} tablet:hidden`}
 			>
 				<Sidebar />
 			</div>
 
-			<div className="text-white text-subtitleMobile font-jost h-14 bg-dark-grey-blue flex items-center justify-between px-6">
+			<div className="text-white text-subtitleMobile font-jost h-14 bg-dark-grey-blue flex items-center justify-between pl-6 pr-6 tablet:h-72px tablet:justify-normal tablet:rounded-lg tablet:gap-38px tablet:pr-2">
+				<div className="hidden tablet:flex items-center">
+					<Suggestions />
+					<span className="text-title18px font-bold m-4">
+						{suggestions.length} Suggestions
+					</span>
+				</div>
 				<div className="flex gap-2 ">
 					<Dropdown options={options} defaultSelectedOption={options[0]} />
 				</div>
-				<Link to={'/new-feedback'}>
-					<Button buttonText={buttonText} buttonClass={buttonClass} />
-				</Link>
+				<div className="tablet:grow text-end">
+					<Link to={'/new-feedback'}>
+						<Button buttonText={buttonText} buttonClass={buttonClass} />
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
