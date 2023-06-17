ee1e4deee9ddd8f44de327d16505d4dbef2c6414
ismaelpaul <ismaafp@gmail.com>
2023-06-17 22:14:48 +0100

    Add condition to stroke color

--- src/components/SVGComponents/ArrowUp.tsx
+++ src/components/SVGComponents/ArrowUp.tsx
@@ -1,14 +1,15 @@
 interface ArrowUpProps {
 	className?: string;
+	hasVoted?: boolean;
 }
 
-const ArrowUp = ({ className }: ArrowUpProps) => {
+const ArrowUp = ({ className, hasVoted }: ArrowUpProps) => {
 	return (
 		<svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
 			<path
 				className={className}
 				d="M1 6l4-4 4 4"
-				stroke="#4661E6"
+				stroke={hasVoted ? '#FFFFFF' : '#3A4374'}
 				strokeWidth="2"
 				fill="none"
 				fillRule="evenodd"
