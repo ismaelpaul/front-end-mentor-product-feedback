81380f228d9a14bf8d9ddfea604f771ac73e3898
ismaelpaul <ismaafp@gmail.com>
2023-06-24 20:42:39 +0100

    Add hasReplies and isLastComment variable

--- src/components/Comments/CommentsList.tsx
+++ src/components/Comments/CommentsList.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { ProductRequests } from '../../interfaces/IProductRequests';
+import { Comments, ProductRequests } from '../../interfaces/IProductRequests';
 import Card from '../Card/Card';
 import ReplyingTo from '../ReplyingTo/ReplyingTo';
 import UserInfo from '../UserInfo/UserInfo';
@@ -8,14 +8,12 @@ import ReplyComment from '../ReplyComment/ReplyComment';
 
 type FeedbackCardPros = {
 	singleRequest: ProductRequests;
+	comments: Comments[];
 };
 
-const CommentsList = ({ singleRequest }: FeedbackCardPros) => {
+const CommentsList = ({ singleRequest, comments }: FeedbackCardPros) => {
 	const [commentIndex, setCommentIndex] = useState(-1);
 	const [replyIndex, setReplyIndex] = useState(-1);
-
-	const comments = singleRequest.comments ?? [];
-
 	const buttonClass =
 		'text-blue text-right font-semiBold text-subtitleMobile basis-0 ml-auto';
 	const buttonText = 'Reply';
@@ -32,15 +30,19 @@ const CommentsList = ({ singleRequest }: FeedbackCardPros) => {
 		);
 	}, [singleRequest.comments]);
 
+	const totalReplies = repliesCount();
+
 	return (
 		<main className="m-6">
 			<Card cardClass={cardClass}>
 				<div className="p-6 tablet:px-8">
 					<h1 className="text-dark-slate-blue text-title18px font-bold mb-6">
-						{comments.length + repliesCount() + ' ' + 'Comments'}
+						{comments.length + totalReplies + ' ' + 'Comments'}
 					</h1>
 					<>
 						{comments.map((comment, index) => {
+							const hasReplies = comment.replies && comment.replies.length > 0;
+							const isLastComment = index === comments.length - 1;
 							return (
 								<>
 									<div className="flex flex-col tablet:gap-4">
@@ -57,7 +59,7 @@ const CommentsList = ({ singleRequest }: FeedbackCardPros) => {
 										<div className="tablet:flex tablet:ml-4">
 											<div
 												className={`border-solid border-l-[1.5px] border-light-slate-blue ${
-													index === comments.length - 1 ? 'h-auto' : 'hidden'
+													hasReplies ? 'h-auto' : 'hidden'
 												} opacity-10 `}
 											></div>
 											<p className="text-light-slate-blue text-subtitleMobile mb-6 mt-4 tablet:ml-[3.5rem] tablet:text-text15px tablet:mt-0">
@@ -66,11 +68,6 @@ const CommentsList = ({ singleRequest }: FeedbackCardPros) => {
 										</div>
 									</div>
 									{index === commentIndex && <ReplyComment />}
-									{index === comments.length - 1 ? (
-										<></>
-									) : (
-										<hr className="bg-regent-grey border-none h-px opacity-25 mb-6" />
-									)}
 									{comment.replies !== undefined
 										? comment.replies.map((reply, index) => {
 												return (
@@ -84,6 +81,11 @@ const CommentsList = ({ singleRequest }: FeedbackCardPros) => {
 												);
 										  })
 										: null}
+									{isLastComment ? (
+										<></>
+									) : (
+										<hr className="bg-regent-grey border-none h-px opacity-25 mb-6" />
+									)}
 								</>
 							);
 						})}
